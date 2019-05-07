fc59f50f36020a070890d737f1875630678b059e
juanjo <31669195+GrupoWeb@users.noreply.github.com>
2019-05-07 16:12:16 -0600

    jj

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,6 @@ Route::any('/operation', function (Request $request) {
     }else if(strpos($query, 'update')!== false){
       $op=DB::update($query);
       return "registro actualizado correctamente";
-    } else if(strpos($query, 'show')!== false){
-      $op=DB::select($query);
-      return dd($op[0]->Tables_in_ejemplo);
     } 
     else{
       return "operacion no admitida";
