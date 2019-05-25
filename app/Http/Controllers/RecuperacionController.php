53338556f4c043fd45cf0207d1c8bd9fe5095c39
Austro <jgomez@software-global.com>
2019-05-24 22:15:53 -0600

    cambio 1 grupo4

--- app/Http/Controllers/RecuperacionController.php
+++ app/Http/Controllers/RecuperacionController.php
@@ -37,28 +37,7 @@ class RecuperacionController extends Controller
 
     public function ejecutarSQL(Request $request)
     {
-<<<<<<< HEAD
-        $configDb = [
-            'driver'      => 'mysql',
-            'host'        => env('DB_HOST', '127.0.0.1'),
-            'port'        => env('DB_PORT', '3306'),
-            'database'    => "$request->database",
-            'username'    => env('DB_USERNAME', 'root'),
-            'password'    => env('DB_PASSWORD', '1234'),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset'     => 'utf8',
-            'collation'   => 'utf8_unicode_ci',
-            'prefix'      => '',
-            'strict'      => true,
-            'engine'      => null,
-        ];
-
-        Config::set('database.connections.DB_Serverr', $configDb);
-
-        $conexionSQL = DB::connection('DB_Serverr');
-=======
         $conexionSQL = $this->conexionInformationSchema("$request->database");
->>>>>>> ca7fff4caf2e0f5373ca5e2720a132408ff49bbd
 
         $query = "SELECT ";
 
